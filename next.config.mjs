aa36015d4a4e111a17e82171499d7c20b2b8596e
Hackers-lab <pramod2santosh@gmail.com>
2026-07-20 04:15:12 +0530

    fix: move outputFileTracingExcludes/Includes to top-level config (no longer under experimental)

--- next.config.mjs
+++ next.config.mjs
@@ -10,19 +10,17 @@ const nextConfig = {
     unoptimized: true,
   },
   serverExternalPackages: ["googleapis", "google-auth-library", "googleapis-common"],
-  experimental: {
-    outputFileTracingExcludes: {
-      "*": [
-        "./node_modules/googleapis/build/src/apis/**",
-      ],
-    },
-    outputFileTracingIncludes: {
-      "*": [
-        "./node_modules/googleapis/build/src/apis/sheets/**",
-        "./node_modules/googleapis/build/src/apis/drive/**",
-        "./node_modules/googleapis/build/src/apis/index.js",
-      ],
-    },
+  outputFileTracingExcludes: {
+    "*": [
+      "./node_modules/googleapis/build/src/apis/**",
+    ],
+  },
+  outputFileTracingIncludes: {
+    "*": [
+      "./node_modules/googleapis/build/src/apis/sheets/**",
+      "./node_modules/googleapis/build/src/apis/drive/**",
+      "./node_modules/googleapis/build/src/apis/index.js",
+    ],
   },
 }
 
